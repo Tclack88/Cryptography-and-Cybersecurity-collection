bf0ec36b814633d7c90097d1c64ea5f384ac1ce9
tclack88 <tclack@aol.com>
2025-08-14 20:31:17 +0800

    removed duplicate nested computing_101 dir

--- pwncollege/computing_101/computing_101/2_get_rsi.s
+++
@@ -1,9 +0,0 @@
-; the last challenge of https://pwn.college/computing-101/your-first-program/
-; wherein a "secret" value is red from rsi. It's moved into rdi as the exit code
-; compile with "as get_rsi.s -o get_rsi.o" then "ld get_rsi.o -o exe"
-.intel_syntax noprefix
-.global _start
-_start:
-        mov rdi, rsi
-        mov rax, 60
-        syscall
--- pwncollege/computing_101/computing_101/6_relative-jump.s
+++
@@ -1,8 +0,0 @@
-// This code is essentially jumping to a an address 0x51 instructions away. We need to fill it in with something to avoid entering some other section (beyond the .text section), so we fill it with nops. the .rept directive allows us to do that without writing forever
-.intel_syntax noprefix
-jmp target
-.rept 0x51
-nop
-.endr
-target:
-mov rax, 1
